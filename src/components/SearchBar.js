35c9c9a893f5cf259fde65f39bb1771b2580ae77
MaryF <filardimary@gmail.com>
2023-02-28 17:03:37 +0100

    search feature implemented

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import styles from '../styles/SearchBar.module.css'
 
-export default function SearchBar() {
+export default function SearchBar({ search, setSearch, searchBook }) {
+
     return (
-        <div class={styles.searchBox}>
-            <button class={styles.btnSearch}><FaSearch /></button>
-            <input type="text" class={styles.inputSearch} placeholder="Type to Search..." />
+        <div className={styles.searchBox}>
+            <button className={styles.btnSearch}><FaSearch /></button>
+            <input
+                type="text" className={styles.inputSearch}
+                placeholder="Type to Search..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={searchBook} />
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import SearchBar from '../components/SearchBar'
 
 
 export default function Home() {
+    const [search, setSearch] = useState('')
+    const [books, setBooks] = useState([])
+
+    const searchBook = (e) => {
+        if (e.key === 'Enter') {
+            fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyB8aM5bhf7EoM3pGpZ6-jpPdGUFDs5PDfU`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    setBooks(data.items)
+                })
+        }
+    }
+
     return (
         <main>
             <div className={styles.mainContainer}>
                 <h1>Find your Books</h1>
                 <h2>And add them to your</h2>
                 <h2>Online Library</h2>
-                <SearchBar />
+                <SearchBar search={search} setSearch={setSearch} searchBook={searchBook} />
             </div>
             <div className={styles.searchResults}>
-                <div className={styles.card}>
-                    <img src="https://via.placeholder.com/300" alt="book cover" />
-                    <h1 className={styles.bookTitle}>Title</h1>
-                    <h2 className={styles.bookAuthor}>Author</h2>
-                    <button>Add Book</button>
-                </div>
-                <div className={styles.card}>
-                    <img src="https://via.placeholder.com/300" alt="book cover" />
-                    <h1>Title</h1>
-                    <h2>Author</h2>
-                    <button>Add Book</button>
-
-                </div>
-                <div className={styles.card}>
-                    <img src="https://via.placeholder.com/300" alt="book cover" />
-                    <h1>Title</h1>
-                    <h2>Author</h2>
-                    <button>Add Book</button>
-
-                </div>
+                {books.map(book => (
+                    <div className={styles.card} key={book.id}>
+                        <img src={book.volumeInfo.imageLinks.thumbnail} alt="book cover" />
+                        <h1 className={styles.bookTitle}>{book.volumeInfo.title}</h1>
+                        <h2 className={styles.bookAuthor}>{book.volumeInfo.title}</h2>
+                        <button>Add Book</button>
+                    </div>
+                ))}
             </div>
         </main>
     )
