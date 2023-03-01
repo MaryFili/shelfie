e4a72571b638f778d56bb4d09e373cd0da2b3d87
MaryF <filardimary@gmail.com>
2023-03-01 15:36:30 +0100

    book saved in db and retrieved from db and possibility to update them

--- src/Layout/RootLayout.js
+++ src/Layout/RootLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import styles from '../styles/RootLayout.module.css'
+// import styles from '../styles/RootLayout.module.css'
 import Header from '../components/Header';
 
 export default function RootLayout() {
---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCxK58v3HYqvCUzKGl-L8rE7Z3p4PDPItM",
+    authDomain: "shelfie-d120d.firebaseapp.com",
+    projectId: "shelfie-d120d",
+    storageBucket: "shelfie-d120d.appspot.com",
+    messagingSenderId: "119997913677",
+    appId: "1:119997913677:web:504c1ffe1b860267689a04",
+    measurementId: "G-48H6B2CHVJ"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const db = getFirestore()
+
+const storage = getStorage();
+
+export { db, storage };
