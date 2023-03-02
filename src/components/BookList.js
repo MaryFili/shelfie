cbcd6a95b6ec1d78f4ce77a0c97ae84c51d55a87
MaryF <filardimary@gmail.com>
2023-03-02 12:50:06 +0100

    modal and style for updating book status

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css';
-
+// import Modal from './Modal';
 
 
 export default function BookList({ bookShelf }) {
-    console.log(bookShelf);
+    const [isLent, setIsLent] = useState(bookShelf.map(book => book.isLent));
+    const [lentToWhom, setLentToWhom] = useState(bookShelf.map(book => book.lentToWhom))
+    const [lentWhen, setLentWhen] = useState(bookShelf.map(book => book.lentWhen))
+    const [isBack, setIsBack] = useState(bookShelf.map(book => book.isBack));
+    const [isRead, setIsRead] = useState(bookShelf.map(book => book.isRead));
+    const [openModal, setOpenModal] = useState(false);
 
     return (
+
         <div className={styles.bookList}>
             {bookShelf.map(book => (
                 <div className={styles.card} key={book.id}>
@@ -19,24 +25,58 @@ export default function BookList({ bookShelf }) {
 
                     <h4 className={styles.bookAuthor}> {book.bookAuthor}</h4>
 
-
-
-
                     {book.isRead && <p>Read</p>}
-                    {!book.isRead && <p>Not yet Read</p>}
+                    {!book.isRead && <p>Not Read</p>}
 
                     {book.isLent &&
                         <div>
                             <p>Book Lent To:</p>
-                            <input type="text"
-                                placeholder='Book lent to'
-                                value={book.lentToWhom} />
-                            <input type="date"
-                                value={book.lentWhen} />
+                            <p>{book.lentToWhom}</p>
+                            <p>{book.lentWhen}</p>
                         </div>
                     }
+                    <button className={styles.openModalBtn} onClick={() => setOpenModal(true)}>Update your Book</button>
+                    {openModal && <div className={styles.modalBackground}>
+                        <div className={styles.modalContainer}>
+                            <div className={styles.closeModal}>
+                                <button onClick={() => setOpenModal(false)}>X</button>
+                            </div>
+                            <div className={styles.readStatus}>
+                                <h2>Have you read this book?</h2>
+                                <div className={styles.readBtn}>
+                                    <button onClick={() => setIsRead(true)}>Yes</button>
+                                    <button onClick={() => setIsRead(false)}>Not yet</button>
+                                </div>
 
+                            </div>
+                            <div className={styles.lentStatus}>
+                                <h2>Have you lent this book to someone?</h2>
+                                <div className={styles.lentBtn}>
+                                    <button onClick={() => setIsLent(true)}>Yes</button>
+                                    <button onClick={() => setIsLent(false)}>Nope</button>
+                                </div>
+                                {isLent &&
+                                    <div className={styles.lentQuestions}>
+                                        <input
+                                            type='text' placeholder='Lent To'
+                                            // value={book.lentToWhom}
+                                            onChange={(e) => setLentToWhom(e.target.value)}
+                                        />
+                                        <input
+                                            type='text' placeholder='Lent When'
+                                            // value={book.lentWhen}
+                                            onChange={(e) => setLentWhen(e.target.value)}
+                                        />
+                                        <div className={styles.saveBtn}>
+                                            <button onClick={() => console.log(book)}>Save</button>
 
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    }
                 </div>
             ))}
         </div>
