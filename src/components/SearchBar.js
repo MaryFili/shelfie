60c040189bc1ba484ac55dea2f30b4754d7a9dbc
MaryF <filardimary@gmail.com>
2023-03-14 17:31:12 +0100

    last changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import styles from '../styles/SearchBar.module.css'
 
-export default function SearchBar({ search, setSearch, searchBook }) {
+export default function SearchBar({ search, setSearch, searchBook, searchOnClick }) {
 
     return (
         <div className={styles.searchBox}>
-            <button className={styles.btnSearch}><FaSearch /></button>
+            <button className={styles.btnSearch} onClick={searchOnClick}><FaSearch /></button>
             <input
                 type="text" className={styles.inputSearch}
                 placeholder="Type to Search..."
