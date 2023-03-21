4a4c6fca12f271677fbd97f2352b17a3423ae544
MaryF <filardimary@gmail.com>
2023-03-21 16:33:53 +0100

    login and signup added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,8 +22,13 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
+//initialize Firestore
 const db = getFirestore()
 
+//initialize storage
 const storage = getStorage();
 
-export { db, storage };
+//initialize auth
+const auth = getAuth()
+
+export { db, storage, auth };
